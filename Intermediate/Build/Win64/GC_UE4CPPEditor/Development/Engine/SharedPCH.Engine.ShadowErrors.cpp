7fdc3c8a90899c6dd0b1cca2678172b911cc5a5c
AlexisRMD <aremond@gaming.tech>
2021-10-07 15:33:41 +0200

    Wrong gitignore at first, trying to resolve the issues

--- Intermediate/Build/Win64/GC_UE4CPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-// Compiler: 14.29.30136
-#include "C:/Users/aremond/Documents/GitHub/UE4InfiltrationGame/Intermediate/Build/Win64/GC_UE4CPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
-=======
-// Compiler: 14.29.30133
-#include "C:/Users/bcanu/Documents/GitHub/UE4InfiltrationGame/Intermediate/Build/Win64/GC_UE4CPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
->>>>>>> PrototypeIA
--- Source/GC_UE4CPP/GC_UE4CPPGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "GC_UE4CPPGameModeBase.h"
-
